3680d0d436eaf07f56612b8470efee3b74a8d961
Matt Skalecki <ikcelaks@gmail.com>
2024-03-09 12:43:15 -0500

    latest version

--- keyboards/moonlander/keymaps/ikcelaks/rules.mk
+++ keyboards/moonlander/keymaps/ikcelaks/rules.mk
@@ -11,7 +11,9 @@ CAPS_WORD_ENABLE = yes
 SRC += sequence_transform/sequence_transform.c
 SRC += sequence_transform/utils.c
 SRC += sequence_transform/keybuffer.c
+SRC += sequence_transform/key_stack.c
 SRC += sequence_transform/trie.c
+SRC += sequence_transform/cursor.c
 
 # Automatically rebuild my sequence_transform rules from the latest config
 $(shell python3 /home/qmk/qmk_userspace/keyboards/moonlander/keymaps/ikcelaks/sequence_transform/generator/sequence_transform_data.py -q)

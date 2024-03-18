742545f63b12ef6990adc0d14e7bea3e583e37fb
Matt Skalecki <ikcelaks@gmail.com>
2024-03-18 13:49:16 -0400

    latest changes

--- keyboards/moonlander/keymaps/ikcelaks/rules.mk
+++ keyboards/moonlander/keymaps/ikcelaks/rules.mk
@@ -4,13 +4,13 @@ CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 SPACE_CADET_ENABLE = no
 COMBO_ENABLE = yes
-REPEAT_KEY_ENABLE = yes
-DEFERRED_EXEC_ENABLE = yes
+REPEAT_KEY_ENABLE = no
+DEFERRED_EXEC_ENABLE = no
 CAPS_WORD_ENABLE = yes
+
+TOP_SYMBOLS=10
 
 include /home/qmk/qmk_userspace/keyboards/moonlander/keymaps/ikcelaks/sequence_transform/rules.mk
 
 # Automatically rebuild my sequence_transform rules from the latest config
 $(shell python3 /home/qmk/qmk_userspace/keyboards/moonlander/keymaps/ikcelaks/sequence_transform/generator/sequence_transform_data.py -q)
-
-TOP_SYMBOLS=10

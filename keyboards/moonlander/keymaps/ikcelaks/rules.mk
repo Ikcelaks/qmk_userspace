e836983226ca98b2c1415c0d2ae7b288550ab0f2
Matt Skalecki <ikcelaks@gmail.com>
2024-05-28 11:27:38 -0400

    simplify makefile setup

--- keyboards/moonlander/keymaps/ikcelaks/rules.mk
+++ keyboards/moonlander/keymaps/ikcelaks/rules.mk
@@ -13,11 +13,5 @@ TOP_SYMBOLS=10
 
 # sequence_transform setup
 mkfile_dir := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
-
-all: st_build
-
-st_build:
-	python3 $(mkfile_dir)/sequence_transform/generator/sequence_transform_data.py
-
 include $(mkfile_dir)/sequence_transform/rules.mk
 # end sequence_transform setup
